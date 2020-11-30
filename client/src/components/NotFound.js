994c77fca8d87391343d0a23c3cdb82b611f3c66
RobertMJr <25443406+RobertMJr@users.noreply.github.com>
2020-11-30 10:00:18 +0200

    Added a button to allow the user to return back to the list of courses

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound =  () => (
     <div className="bounds">
         <h1> Not Found</h1>
         <p>Sorry! We couldn't find the page you're looking for.</p>
+        <span>
+            <Link className="button button-secondary" to="/">Return to List</Link>
+        </span>
     </div>
 );
 
