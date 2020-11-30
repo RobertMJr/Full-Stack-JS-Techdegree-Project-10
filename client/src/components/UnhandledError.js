0f5861e1245c9c68fd7e94cc4a0abfda0ff821dd
RobertMJr <25443406+RobertMJr@users.noreply.github.com>
2020-11-30 10:00:32 +0200

    Added a button to allow the user to return back to the list of courses

--- client/src/components/UnhandledError.js
+++ client/src/components/UnhandledError.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const UnhandledError = () => {
-    return (
+const UnhandledError = () => (
         <div className="bounds">
             <h1>Error</h1>
             <p>Sorry! We just encountered an unexpected error.</p>
+            <span>
+                <Link className="button button-secondary" to="/">Return to List</Link>
+            </span>
         </div>
     );
-}
 
 export default UnhandledError;
