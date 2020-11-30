a73e2a75896d5d35d74d048bb754e1dfeded54f1
RobertMJr <25443406+RobertMJr@users.noreply.github.com>
2020-11-30 10:00:04 +0200

    Added a button to allow the user to return back to the list of courses

--- client/src/components/Forbidden.js
+++ client/src/components/Forbidden.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Forbidden =  () => (
     <div className="bounds">
         <h1>Forbidden</h1>
         <p>Oh oh! You can't access this page.</p>
+        <span>
+            <Link className="button button-secondary" to="/">Return to List</Link>
+        </span>
     </div>
 );
 
